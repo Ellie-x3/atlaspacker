28baea805d7f21f5e153e15cc43f1153f14d81dd
Ellie-x3 <troymaxwell55@gmail.com>
2025-04-04 20:53:56 -0300

    init commit
    init commit

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,2 +1,10 @@
 class_name Menu
 extends Control
+
+@onready var _create_button: Button = %Button
+
+func _init() -> void:
+	RenderingServer.set_default_clear_color(Color8(19,23,36))
+
+func _ready() -> void:
+	_create_button.grab_focus()
