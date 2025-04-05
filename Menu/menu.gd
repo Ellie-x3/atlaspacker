33760c44c474c97f1802243c7c4caff5694095ed
Ellie-x3 <troymaxwell55@gmail.com>
2025-04-04 21:47:38 -0300

    a
    a

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -4,7 +4,10 @@ extends Control
 @onready var _create_button: Button = %Button
 
 func _init() -> void:
-	RenderingServer.set_default_clear_color(Color8(19,23,36))
+	RenderingServer.set_default_clear_color(Color8(45,45,45))
 
 func _ready() -> void:
 	_create_button.grab_focus()
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Views/Main/project_view.tscn")
---
+++ Views/Main/project_view.gd
@@ -0,0 +1,5 @@
+class_name ProjectView
+extends Node2D
+
+func _init() -> void:
+    RenderingServer.set_default_clear_color(Color8(220,220,220))
